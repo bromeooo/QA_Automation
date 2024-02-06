6b142c76d067dd518cae493a36391bedc8b7e37a
bromeooo <67499389+bromeooo@users.noreply.github.com>
2024-02-06 12:47:07 -0700

    add tracer

--- web/javascript/testCucumber/config/after.js
+++ web/javascript/testCucumber/config/after.js
@@ -1,6 +1,10 @@
 const { After } = require('@cucumber/cucumber');
 
-After(async function() {
+After(async function (scenario) {
+  if (this.context) {
+    const scenarioName = scenario.pickle.name.replace(/[^a-zA-Z0-9]/g, '_'); // Sanitize scenario name to use in directory name
+    await this.context.tracing.stop({ path: `./testArtifacts/trace/${scenarioName}` });
+  }
   if (this.browser) {
     await this.browser.close();
   }
