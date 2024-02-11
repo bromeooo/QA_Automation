2c9b23d4b91345902366b06e3d5d3ff53368deba
bromeooo <67499389+bromeooo@users.noreply.github.com>
2024-02-10 20:15:58 -0700

    fix amex test

--- web/python/cucumber/driver_playwright/amex_interview/fill_job.py
+++ web/python/cucumber/driver_playwright/amex_interview/fill_job.py
@@ -22,19 +22,20 @@ class FillJob(BasePage):
         self.no_resume_click = self.page.locator(
             '[name="editTemplateMultipart-editForm-content-ftf-saveContinueCmdBottom"]'
         )
-
+        # hear about us dropdown
+        self.hear_about = self.page.locator("xpath=//select[contains(@id, 'Block-recruitmentSourceType')]");
+        self.about_select = self.page.locator("xpath=//select[contains(@id, 'Block-recruitmentSourceType')]/option[text()='Agency']");
         # Contact Information
         self.job_notification_checkbox = (
             self.page.get_by_text("Job Notification")
             .locator("../..")
             .locator("[type='checkbox']")
-        ) # this locator showcases the power of playwright 
+        )  # this locator showcases the power of playwright
         self.no_resume_click = self.page.locator(
             '[name="editTemplateMultipart-editForm-content-ftf-saveContinueCmdBottom"]'
         )
-        self.first_name = self.page.locator(
-            '[name="eet-ef-content-ftf-gp-j_id_id16pc9-page_2-cpi-cfrmsub-frm-dv_cs_candidate_personal_info_FirstName"]'
-        )
+        self.first_name = self.page.locator("xpath=//*[contains(@id, 'candidate_personal_info_FirstName')]");
+
         self.last_name = self.page.locator(
             '[name="et-ef-content-ftf-gp-j_id_id16pc9-page_2-cpi-cfrmsub-frm-dv_cs_candidate_personal_info_LastName"]'
         )
@@ -50,6 +51,9 @@ class FillJob(BasePage):
         self.phone = self.page.locator(
             '[name="et-ef-content-ftf-gp-j_id_id16pc9-page_2-cpi-cfrmsub-frm-dv_cs_candidate_personal_info_HomePhone"]'
         )
+        self.title = self.page.locator("[class='titlepage']")
+        self.country = self.page.locator("xpath=//select[contains(@onchange, 'personal_info_ResidenceLocation') and not(@disabled)]")
+
 
 
     async def continue_no_account(self):
@@ -64,14 +68,18 @@ class FillJob(BasePage):
     #     await self.do_click(self.no_resume_click)
 
     async def fill_exp(self):
+        await self.hear_about.select_option(label='Agency')
 
-        
         # Click on the job notification checkbox or perform other actions as needed
         await self.do_click(self.job_notification_checkbox)
-        await self.send_keys(self.first_name, "Brian")
+        await self.first_name.click()
+        await self.first_name.fill("Brian")
         await self.send_keys(self.last_name, "Romero")
         await self.send_keys(self.street_address, "18 Not Real Street")
         await self.send_keys(self.city, "Chandler")
         await self.send_keys(self.zip, "85225")
-        pdb.set_trace()
+        await self.send_keys(self.phone, "8562309432")
+        await self.country.select_option(label='United States')
+        return await self.title.text_content()
+   
 
--- web/python/cucumber/features/amex_interview.feature
+++ web/python/cucumber/features/amex_interview.feature
@@ -1,8 +1,8 @@
-# Feature: Amex Interview
+Feature: Amex Interview
 
-#   @playwright @amex
-#   Scenario: Playwright Amex Interview
-#     Given I open the job posting
-#     When I fill out the job application
-#     Then I get the job
+  @playwright @amex
+  Scenario: Playwright Amex Interview
+    Given I open the job posting
+    When I fill out the job application
+    Then I get the job
 
